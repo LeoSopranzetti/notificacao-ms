4fd78f4009d9cc47dfa824e6e74b42113fb2fe74
Leonardo <leo.sopranzetti@hotmail.com>
2024-07-06 12:40:58 -0300

    init

---
+++ src/main/java/tech/leo/notificacao_ms/NotificacaoMsApplication.java
@@ -0,0 +1,13 @@
+package tech.leo.notificacao_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoMsApplication.class, args);
+	}
+
+}
